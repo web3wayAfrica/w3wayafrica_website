b52cee610002f8c3f0d42d3fee3dda7bbcff0e19
Cherrypick14 <cherrypicklet@gmail.com>
2025-09-23 22:25:34 +0300

    chore: update Astro config integrations

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
-
+  
   integrations: [
     tailwind({
       applyBaseStyles: false,
